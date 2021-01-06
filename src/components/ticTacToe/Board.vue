39fc6be085d0d60149049b9828418007591fe39e
Kazakov Mihail <advancerman@gmail.com>
2021-01-06 16:09:19 +0700

    Putted pics to Board instead of colored divs

--- src/components/ticTacToe/Board.vue
+++ src/components/ticTacToe/Board.vue
@@ -1,10 +1,19 @@
 <template>
     <table class="tic-tac-toe-table">
+        <thead hidden v-if="colors !== undefined">
+        <!-- hidden images to load them at page init -->
+        <tr v-for="i in colors.length" :key="i">
+            <th>
+                <img :src="picPath(i - 1)" alt=""/>
+            </th>
+        </tr>
+        </thead>
         <tbody>
         <tr v-for="i in board.length" :key="i">
             <td v-for="j in board[i - 1].length" :key="j"
                 @click="$emit('makeTurn', i - 1, j - 1)">
-                <div :style="styles[board[i - 1][j - 1]]"
+                <img :src="picPath(board[i - 1][j - 1])"
+                     :alt="`player ${board[i - 1][j - 1] + 1}`"
                      v-if="board[i - 1][j - 1] !== -1"/>
                 <div v-else/>
             </td>
@@ -17,10 +26,11 @@
 export default {
     name: "Board",
     props: ['board', 'colors'],
-    computed: {
-        styles() {
-            return this.colors.map(color => 'background-color: ' + color);
-        }
+    methods: {
+        picPath(i) {
+            const type = i % 2 ? 'circle' : 'cross';
+            return `/api/v1/ticTacToe/pics/${type}/${this.colors[i].slice(1)}`;
+        },
     }
 }
 </script>
@@ -29,5 +39,9 @@ export default {
 .tic-tac-toe-table tr td *:first-child {
     width: var(--tic-tac-toe-cell-size);
     height: var(--tic-tac-toe-cell-size);
+}
+
+.tic-tac-toe-table tr td img {
+    vertical-align: top;
 }
 </style>
